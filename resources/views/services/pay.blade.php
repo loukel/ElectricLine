7393e245299aaac22b7ed3b3b74dba9c837a443f
louis <Louis.Kelly1@hotmail.com>
2021-01-23 15:10:01 +0000

    Set up booking.pay

--- resources/views/services/pay.blade.php
+++ resources/views/services/pay.blade.php
@@ -4,14 +4,40 @@
 <div class="container py-4 w-50 d-flex justify-content-center flex-wrap">
   <div class="card p-5 w-75">
     <div class="card-body">
-      <div class="card-title h3">Pay Methods</div>
+      <div class="card-title h3">Payment Method</div>
     </div>
   </div>
   <div class="card p-5 w-75 mt-4">
     <div class="card-body">
       <div class="card-title h3">Booking details</div>
-      <label for="service">Service</label>
-      <span class="h5 text-muted">{{ request()->route('slug') }}</span>
+      <div>
+        <label for="service">Service</label>
+        <span class="h5 text-muted"></span>
+      </div>
+      <div>
+        <label for="service">Name</label>
+        <span class="h5 text-muted"></span>
+      </div>
+      <div>
+        <label for="service">Address</label>
+        <span class="h5 text-muted"></span>
+      </div>
+      <div>
+        <label for="service">Provider</label>
+        <span class="h5 text-muted"></span>
+      </div>
+      <div>
+        <label for="service">Date</label>
+        <span class="h5 text-muted"></span>
+      </div>
+      <div>
+        <label for="service">Time</label>
+        <span class="h5 text-muted"></span>
+      </div>
+      <div>
+        <label for="service">Booking Total</label>
+        <span class="h5 text-muted"></span>
+      </div>
       <button class="btn btn-success">Pay and Confirm</button>
     </div>
   </div>
