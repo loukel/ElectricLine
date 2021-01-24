4759e030028c4de8cad0a2155f078a4cd2921bba
louis <Louis.Kelly1@hotmail.com>
2021-01-24 18:40:52 +0000

    Added controller and model

--- app/Http/Controllers/BookingController.php
+++ app/Http/Controllers/BookingController.php
@@ -4,9 +4,14 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\Booking;
+
 class BookingController extends Controller
 {
   public function create($slug) {
+    // $booking = new Booking();
+    request('postcode');
+
     $bookingID = 1;
 
     return redirect(route('booking.pay', $slug, compact($bookingID)));
