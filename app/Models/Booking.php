b81857259995a0a20a4505467f52a738f885a529
louis <Louis.Kelly1@hotmail.com>
2021-01-24 19:20:18 +0000

    Created the relationship functions for the booking model

--- app/Models/Booking.php
+++ app/Models/Booking.php
@@ -5,7 +5,33 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Booking extends Model
-{
-    use HasFactory;
+class Booking extends Model {
+  use HasFactory;
+  /**
+   * Get the address of the location
+   */
+  public function address() {
+    return $this->belongsTo(Address::class);
+  }
+
+    /**
+   * Get the customer who made the booking
+   */
+  public function customer() {
+    return $this->belongsTo(Customer::class);
+  }
+
+    /**
+   * Get the service variant
+   */
+  public function service_variant() {
+    return $this->belongsTo(Provider::class);
+  }
+
+    /**
+   * Get the provider who is completing the booking
+   */
+  public function provider() {
+    return $this->belongsTo(ServiceVariant::class);
+  }
 }
