4b84c184b034dfc776ebecb65c8580ca27e84873
louis <Louis.Kelly1@hotmail.com>
2021-01-24 19:05:04 +0000

    Fixed another typo on the same table

--- database/migrations/2021_01_24_170638_create_service_variants_table.php
+++ database/migrations/2021_01_24_170638_create_service_variants_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateSeviceVariantsTable extends Migration
+class CreateServiceVariantsTable extends Migration
 {
     /**
      * Run the migrations.
